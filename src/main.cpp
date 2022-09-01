8e10c34c5912972bbef73a872fe4f592728dffcd
João Pedro Martins Espíndola <87656175+JoaoMEspindola@users.noreply.github.com>
2022-09-01 20:03:37 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ int main(){
     char coordenadas[100], escolha;
     stringstream stream;
     string coordenadasTokenizadas;
+    unordered_map <string, int**> map;
 
     cout << "Qual o tamanho da matriz quadrada desejada?" << endl;
     cin >> tam;
@@ -15,7 +16,7 @@ int main(){
     arquivaMatriz(matriz, tam);
 
     do{
-
+        stream.str("");
         cout << "Em quais coordenadas deseja criar uma submatriz? (Escrever no padrão: i,j/i,j)";
         cin >> coordenadas;
 
@@ -27,11 +28,27 @@ int main(){
             token = strtok(NULL, "/,");
         }
 
-        stream >> coordenadasTokenizadas;
+        coordenadasTokenizadas =  stream.str();
 
-        matrizResultante = recortaMatriz (arr);
+        cout << "COORDENADA TOKENIZAADA : " << coordenadasTokenizadas << endl;
 
-        hashMatriz(coordenadasTokenizadas, matrizResultante);
+        if (!(verificaMatriz(map, coordenadasTokenizadas))){
+            
+            if (tam < 15){
+                cout << "Matriz principal gerada: " << endl;
+                imprimeMatriz(matriz, tam, tam);
+            }else{
+                cout << "Matriz principal muito grande para imprimir no terminal. Por favor, consulte o arquivo 'matrix.txt'" << endl;
+            }
+            
+            matrizResultante = recortaMatriz (arr);
+
+            map.emplace(coordenadasTokenizadas, matrizResultante);
+        }else{
+            cout << endl << "Este recorte já foi feito, e o resultado é: " << endl;
+            imprimeMatriz(map[coordenadasTokenizadas], (arr[2] - arr[0])+1, (arr[3] - arr[1])+1);
+        }
+
 
         cout << "Deseja criar outra submatriz? Sim(S), Não(N)." << endl;
         cin >> escolha;
